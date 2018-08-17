2b8396f323c34d41f194bd822d8bf347ad97bd6c
mthom26 <mthom2626@gmail.com>
2018-08-17 11:41:14 +0100

    Initial setup

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/reboot.css';
+import '../styles/main.scss';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Car Body Repair</h1>
+    </div>
+  );
+};
+
+export default Index;
