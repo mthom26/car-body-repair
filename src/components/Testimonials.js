c66d7017713e2b597f9cecd0f35e1ad8750b4f97
mthom26 <mthom2626@gmail.com>
2018-09-03 15:02:11 +0100

    Add Testimonials carousel

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import Img from 'gatsby-image';
+import {
+  CarouselProvider,
+  ButtonBack,
+  ButtonNext,
+  Slider,
+  Slide
+} from 'pure-react-carousel';
+
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const Testimonials = ({ image }) => {
   //console.log(image)
   return (
     <div className="testimonials">
       <div className="testimonialsContent text-center">
-        <h2>Customer Comments</h2>
-        <p>Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue.</p>
-        <p>Mr. Big Noodle</p>
+        <h2>Customer Testimonials</h2>
+        <CarouselProvider
+          naturalSlideHeight={100}
+          naturalSlideWidth={150}
+          totalSlides={3}
+          isPlaying={false}
+          interval={5000}
+          className="cprov"
+        >
+          <Slider>
+            <Slide innerClassName="slide" index={0}>
+              <p>Excellent and timely service. Thanks Auto Body Repairs!</p>
+              <hr className="w-75" />
+              <p>Mr A. Davis</p>
+            </Slide>
+            <Slide innerClassName="slide" index={1}>
+              <p>Prompt high quality service. Would recommend to family and friends.</p>
+              <hr className="w-75" />
+              <p>Mr J. McCabe</p>
+            </Slide>
+            <Slide innerClassName="slide" index={2}>
+              <p>Excellent and timely service. Thanks Auto Body Repairs!</p>
+              <hr className="w-75" />
+              <p>Mr T. Jeffries</p>
+            </Slide>
+          </Slider>
+          <ButtonBack>Back</ButtonBack>
+          <ButtonNext>Next</ButtonNext>
+        </CarouselProvider>
       </div>
       
       <Img 
